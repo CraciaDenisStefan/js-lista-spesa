81a29bdfc6fbe329b8e1a9cd1db6f0962361a82c
CraciDenisStefan <crdenis13@gmail.com>
2023-05-24 14:29:05 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+let lista = [
+    `anguria`,
+    `melone`,
+    `pomodoro`,
+    `formaggio`,
+    `pane`,
+    `carne`,
+]
+
+let ul = document.getElementById(`lista_spesa`);
+
+let i = 0;
+while (i < lista.length) {
+    i++;
+   
+    let lista_elem = `<li>${lista[i]}</li>`;
+    ul.innerHTML += lista_elem;
+}
