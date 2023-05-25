68463c53969d0929069938a7ee670fd8a9dd8c45
CraciDenisStefan <crdenis13@gmail.com>
2023-05-25 09:50:36 +0200

    [FIX] i++ messo dopo

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@ let ul = document.getElementById(`lista_spesa`);
 
 let i = 0;
 while (i < lista.length) {
-    i++;
    
     let lista_elem = `<li>${lista[i]}</li>`;
     ul.innerHTML += lista_elem;
+     i++;
 }
